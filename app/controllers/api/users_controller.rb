db76545081a75f361554c57159b7245e3650609b
paulollerton <paul.ollerton@gmail.com>
2019-07-08 13:20:26 -0700

    adds authentication/authorization to userss controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::UsersController < ApplicationController
 
+  before_action :authenticate_user, only: [:update, :destroy] 
+
   def show
     user_id = params[:id]
     @user = User.find_by(id: user_id)
@@ -17,33 +19,40 @@ class Api::UsersController < ApplicationController
     )
 
     if @user.save
-      render json: {message: 'User created successfully'}, status: :created
+      render json: {message: 'User updated successfully'}, status: :created
     else
       render json: {errors: @user.errors.full_messages}, status: :bad_request
     end
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.username = params[:username] || @user.username
-    @user.email = params[:email] || @user.email
-    @user.password = params[:password] || @user.password
-    @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
-    @user.image_url = params[:image_url] || @user.image_url
-    @user.bio = params[:bio] || @user.bio
+    if current_user.id == params[:id].to_i
+      @user = User.find(params[:id])
+      @user.username = params[:username] || @user.username
+      @user.email = params[:email] || @user.email
+      @user.password = params[:password] || @user.password
+      @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
+      @user.image_url = params[:image_url] || @user.image_url
+      @user.bio = params[:bio] || @user.bio
 
-    if @user.save
-      render json: {message: 'User created successfully'}, status: :created
+      if @user.save
+        render json: {message: 'User created successfully'}, status: :created
+      else
+        render json: {errors: @user.errors.full_messages}, status: :bad_request
+      end
     else
-      render json: {errors: @user.errors.full_messages}, status: :bad_request
+      render json: {message: "Please login to edit your profile."}
     end
-    
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    render json: {message: "User Deleted"}
+    if current_user.id == params[:id].to_i
+      @user.destroy
+      render json: {message: "User Deleted"}
+    else
+      render json: {message: "Unauthorized"}
+    end
   end
 
 end
