11eb180ef612993150b9656df2e114bca7fe2886
paulollerton <paul.ollerton@gmail.com>
2019-07-08 13:54:26 -0700

    adds authentication/authorization to votes controller

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -6,26 +6,31 @@ class Api::VotesController < ApplicationController
   end
 
   def create
-
-    @vote = Vote.new(
-      user_id: params[:user_id],
-      recipe_id: params[:recipe_id],
-      suggestion_id: params[:suggestion_id],
-      vote_value: params[:vote_value]
-      )
-
-    if @vote.save
-      render "show.json.jbuilder"
+    if current_user
+      @vote = Vote.new(
+        user_id: params[:user_id],
+        recipe_id: params[:recipe_id],
+        suggestion_id: params[:suggestion_id],
+        vote_value: params[:vote_value]
+        )
+      if @vote.save
+        render "show.json.jbuilder"
+      else
+        render json: {errors: @vote.errors.full_messages}, status: :unprocessable_entity
+      end
     else
-      render json: {errors: @vote.errors.full_messages}, status: :unprocessable_entity
+      render json: {message: "Please login or create an account to vote."}
     end
-
   end
 
   def destroy
     @vote = Vote.find(params[:id])
-    @vote.destroy
-    render json: {message: "Vote Deleted"}
+    if current_user.id == @vote.user_id.to_i
+      @vote.destroy
+      render json: {message: "Vote Deleted"}
+    else
+      render json: {message: "Please login to delete this vote."}
+    end
   end
 
 end
